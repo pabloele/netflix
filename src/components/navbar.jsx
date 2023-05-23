35dc1ecc390a53aae6129dd7206ae904bb57f8d7
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2023-05-23 14:45:07 -0300

    navbar basico

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Navbar() {
-  return <div className="bg-black">NETFLIX</div>;
+  return (
+    <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
+      <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+        NETFLIX
+      </h1>
+      <div>
+        <button className="text-white pr-4">Sign In</button>
+        <button className="text-white bg-red-600 px-6 py-2 rounded cursor-pointer">
+          {" "}
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
 }
