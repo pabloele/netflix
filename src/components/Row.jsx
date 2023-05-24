8d7b5399aa0ce5f05204d0542f6b0ed813568382
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2023-05-24 12:40:38 -0300

    text in rows

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -37,6 +37,13 @@ export default function Row({ title, fetchURL }) {
                 src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`}
                 alt={item.title ? item.title : "img"}
               />
+              <div
+                className="absolute top-0 left-0 w-full h-full hover: bg-black/80 opacity-0 hover:opacity-100
+               text-white">
+                <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center ">
+                  {item?.title}
+                </p>
+              </div>
             </div>
           ))}
         </div>
